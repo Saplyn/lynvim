3855e364f6b22746886c7c9f5a9796ee4ad4f698
saplyn <saplynx@outlook.com>
2024-04-27 00:02:29 +0800

    install lazy.nvim

--- lua/profile/lynvim.lua
+++ lua/profile/lynvim.lua
@@ -1,4 +1,16 @@
 require("config.vimopt")
 require("config.keymap")
 
+--~ Install `lazy.nvim` plugin manager
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+end
+vim.opt.rtp:prepend(lazypath)
 
+--~ Configure `lazy.nvim` plugins
+require('lazy').setup({
+  --~ auto detect tabstop and shiftwidth
+  'tpope/vim-sleuth',
+})
