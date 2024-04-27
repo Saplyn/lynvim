6154b4db6e3e70450fe33f0b95956f78336880fd
saplyn <saplynx@outlook.com>
2024-04-27 22:25:49 +0800

    re-enable line move (problem belongs to tmux, solved)

--- lua/lynvim/config/keymap.lua
+++ lua/lynvim/config/keymap.lua
@@ -16,12 +16,12 @@ vim.keymap.set('n', '<leader><tab>n', '<cmd>tabnext<cr>', { desc = '[N]ext Tab' 
 vim.keymap.set('n', '<leader><tab>p', '<cmd>tabprevious<cr>', { desc = '[P]revious Tab' })
 
 --~ Move lines
--- vim.keymap.set('n', '<M-j>', '<cmd>m .+1<cr>==', { desc = 'Move Line Down' })
--- vim.keymap.set('n', '<M-k>', '<cmd>m .-2<cr>==', { desc = 'Move Line Up' })
--- vim.keymap.set('i', '<M-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move Down' })
--- vim.keymap.set('i', '<M-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
--- vim.keymap.set('v', '<M-j>', ":m '>+1<cr>gv=gv", { desc = 'Move Line Down' })
--- vim.keymap.set('v', '<M-k>', ":m '<-2<cr>gv=gv", { desc = 'Move Line Up' })
+vim.keymap.set('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move Line Down' })
+vim.keymap.set('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move Line Up' })
+vim.keymap.set('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move Down' })
+vim.keymap.set('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
+vim.keymap.set('v', '<A-j>', ":m '>+1<cr>gv=gv", { desc = 'Move Line Down' })
+vim.keymap.set('v', '<A-k>', ":m '<-2<cr>gv=gv", { desc = 'Move Line Up' })
 
 --~ Quality of life
 vim.keymap.set('x', '<leader>p', [["_dP]], { desc = 'Lossless [P]aste' })
