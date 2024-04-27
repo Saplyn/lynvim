ffef2f655266e11980a274083cccd6776db99443
saplyn <saplynx@outlook.com>
2024-04-27 22:58:41 +0800

    shortcut for peek preview markdown

--- lua/lynvim/plugin/peek.lua
+++ lua/lynvim/plugin/peek.lua
@@ -7,5 +7,15 @@ return {
     require('peek').setup()
     vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
     vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+    vim.api.nvim_create_user_command('PeekToggle', function()
+      local peek = require 'peek'
+      if peek.is_open() then
+        peek.close()
+      else
+        peek.open()
+      end
+    end, {})
+
+    vim.keymap.set('n', '<leader>cp', '<cmd>PeekToggle<cr>', { desc = 'Peek: [C]ode [P]review Markdown' })
   end,
 }
