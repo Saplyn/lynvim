4894458900dd9d4a09cfe3e1254bdf4adc5cc355
saplyn <saplynx@outlook.com>
2024-04-28 19:15:52 +0800

    <leader>qq for quit & window resize

--- lua/lynvim/config/keymap.lua
+++ lua/lynvim/config/keymap.lua
@@ -25,3 +25,10 @@ vim.keymap.set('v', '<A-k>', ":m '<-2<cr>gv=gv", { desc = 'Move Line Up' })
 
 --~ Quality of life
 vim.keymap.set('x', '<leader>p', [["_dP]], { desc = 'Lossless [P]aste' })
+vim.keymap.set('n', '<leader>qq', '<cmd>qa<cr>', { desc = '[Q]uit All' })
+
+--~ Resize window
+vim.keymap.set('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
+vim.keymap.set('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
+vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
+vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
