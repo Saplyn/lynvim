edf7d53762fad149931ff388210457129e814340
saplyn <saplynx@outlook.com>
2024-04-28 19:20:41 +0800

    comment & mini

--- lua/lynvim/plugin/init.lua
+++ lua/lynvim/plugin/init.lua
@@ -1,4 +1,12 @@
 return {
   --~ Auto detect tabstop and shiftwidth
   'tpope/vim-sleuth',
+
+  --~ Quick commenting
+  -- n: `gcc` Line-comment toggle
+  -- n: `gbc` Block-comment toggle
+  -- n: 'gcO' Add comment on the line above
+  -- n: 'gco' Add comment on the line below
+  -- n: 'gcA' Add comment at the end of line
+  { 'numToStr/Comment.nvim', opts = {} },
 }
---
+++ lua/lynvim/plugin/mini.lua
@@ -0,0 +1,17 @@
+return {
+  'echasnovski/mini.nvim',
+  config = function()
+    -- n: `va)`  [V]isually select [A]round [)]paren
+    -- n: `yinq` [Y]ank [I]nside [N]ext [']quote
+    -- n: `ci'`  [C]hange [I]nside [']quote
+    require('mini.ai').setup { n_lines = 500 }
+
+    -- n: `saiw)`  [S]urround [A]dd [I]nner [W]ord [)]Paren
+    -- n: `sd'`    [S]urround [D]elete [']quotes
+    -- n: `sr)'`   [S]urround [R]eplace [)] [']
+    require('mini.surround').setup()
+
+    -- Bracket auto-pairing
+    require('mini.pairs').setup()
+  end,
+}
