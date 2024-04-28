a7d038ae319506275bc91a37401e616a358289ae
saplyn <saplynx@outlook.com>
2024-04-28 19:46:12 +0800

    fix: info diagnostic sign

--- lua/lynvim/config/vimopt.lua
+++ lua/lynvim/config/vimopt.lua
@@ -57,7 +57,7 @@ vim.opt.spelllang = 'en_us'
 vim.opt.spell = true
 
 --~ Vim diagnostic sign
-local signs = { Error = ' ', Warn = ' ', Hint = '󰌶 ', Info = '󰋽' }
+local signs = { Error = ' ', Warn = ' ', Hint = '󰌶 ', Info = '󰋽 ' }
 for type, icon in pairs(signs) do
   local hl = 'DiagnosticSign' .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
