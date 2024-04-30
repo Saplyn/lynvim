d042fd70afbead0549ea04106d413f2b7f84e49a
saplyn <saplynx@outlook.com>
2024-04-30 17:22:55 +0800

    give myself a hardtime.nvim

---
+++ lua/lynvim/plugin/hardtime.lua
@@ -0,0 +1,14 @@
+-- lazy.nvim
+return {
+  'm4xshen/hardtime.nvim',
+  dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+  config = function()
+    vim.keymap.set('n', '<leader>H', '<cmd>Hardtime toggle<cr>', { desc = 'Togle [H]ardtime.nvim' })
+
+    require('hardtime').setup {
+      max_count = 5,
+      disable_mouse = false,
+      disabled_filetypes = { 'qf', 'netrw', 'neo-tree', 'lazy', 'mason', 'NeoTree', 'help' },
+    }
+  end,
+}
