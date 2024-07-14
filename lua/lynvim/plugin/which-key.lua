67bfd45d3e7fa81710f41da7cc02538ad608c107
Saplyn Miao <saplynx@outlook.com>
2024-07-15 00:10:15 +1000

    upgrade whick-key config to latest

--- lua/lynvim/plugin/which-key.lua
+++ lua/lynvim/plugin/which-key.lua
@@ -5,14 +5,21 @@ return {
   config = function()
     require('which-key').setup()
 
-    require('which-key').register {
-      ['<leader><tab>'] = { name = '[Tab]page', _ = 'which_key_ignore' },
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>x'] = { name = '[X] Trouble', _ = 'which_key_ignore' },
-      ['<leader>q'] = { name = '[Q] Session', _ = 'which_key_ignore' },
-      ['<leader>m'] = { name = '[M]anage Plugin', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = '[H] Copilot', _ = 'which_key_ignore' },
+    require('which-key').add {
+      { '<leader><tab>', group = '[Tab]page' },
+      { '<leader><tab>_', hidden = true },
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>c_', hidden = true },
+      { '<leader>h', group = '[H] Copilot' },
+      { '<leader>h_', hidden = true },
+      { '<leader>m', group = '[M]anage Plugin' },
+      { '<leader>m_', hidden = true },
+      { '<leader>q', group = '[Q] Session' },
+      { '<leader>q_', hidden = true },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>s_', hidden = true },
+      { '<leader>x', group = '[X] Trouble' },
+      { '<leader>x_', hidden = true },
     }
   end,
 }
